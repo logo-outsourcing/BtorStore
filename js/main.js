86323dacc750083839292a55b8ca59c8e6729d15
= <vikytrusowa.@gmail.com>
2020-02-09 11:52:04 +0200

    editing  adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('.reviews__slider.swiper-container', {
+    var mySwiper = new Swiper('.reviews__slider.swiper-container', {
         // Optional parameters
         pagination: {
             el: '.swiper-pagination',
@@ -9,5 +9,22 @@ $(document).ready(function () {
         },
         slidesPerView: 3,
         spaceBetween: 30,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is >= 480px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
     })
 });
