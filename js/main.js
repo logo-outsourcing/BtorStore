257285ad389ea1a24b063f17398f6fcae43aa55d
= <vikytrusowa.@gmail.com>
2020-02-09 15:40:32 +0200

    editing modal window

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
+
+    // Scroll to section
+
     $('.header__link').click(function () {
         var target = $(this).attr('href');
         $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
         return false;
     });
+
+    //Open modal window
+
     $(".invite__button").fancybox();
-    function open() {
-        $(".invite__send").fancybox.close();
-    }
-    setTimeout(open, 0);
+
+    //Close modal window
+
+    $('.invite__send').on('click',function open () {
+        $.fancybox.close();
+    });
+
+    //Open modal-thank window
+
+    $(".invite__send").fancybox();
+
     //initialize swiper when document ready
+
     var mySwiper = new Swiper('.reviews__slider.swiper-container', {
         // Optional parameters
         pagination: {
@@ -20,17 +34,14 @@ $(document).ready(function () {
         slidesPerView: 3,
         spaceBetween: 30,
         breakpoints: {
-            // when window width is >= 320px
             320: {
                 slidesPerView: 1,
                 spaceBetween: 10
             },
-            // when window width is >= 480px
             768: {
                 slidesPerView: 2,
                 spaceBetween: 20
             },
-            // when window width is >= 640px
             1200: {
                 slidesPerView: 3,
                 spaceBetween: 30
